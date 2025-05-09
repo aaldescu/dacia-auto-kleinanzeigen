5d892ec4a1f1d0aee9e5ea811fb6fbc135b94391
aaldescu <anderi.aldescu@yahoo.com>
2025-05-09 22:54:14 +0200

    Updated data 2025-05-09 22:54:14

--- transform.py
+++ transform.py
@@ -35,11 +35,11 @@ def transform_ad_data(ad):
 
 # --- Database Setup ---
 # Get database connection details from environment variables
-DB_HOST = os.getenv('DB_HOST', 'andreialdescu.com')
-DB_PORT = int(os.getenv('DB_PORT', 3306))
-DB_NAME = os.getenv('DB_NAME', 'otzbgdpw_dacia')
-DB_USER = os.getenv('DB_USER', 'otzbgdpw_dacia')
-DB_PASSWORD = os.getenv('DB_PASSWORD', '')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = int(os.getenv('DB_PORT'))
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
 
 # Function to get database connection
 def get_db_connection():
